eefeb9315e4cb5e9654634a696d65c6be4c90cc4
Nikita <nvadeveloper@gmail.com>
2022-04-03 22:55:42 +0300

    NFT smart contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+const fs = require('fs')
+// const privateKey = fs.readFileSync('./keys/private.pem', 'utf8')
+const privateKey = fs.readFileSync('.secret').toString()
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const projectId = '1839705833d340189785777890b5544a'
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: `https://polygon-mumbai.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+    mainnet: {
+      url: `https://polygon-mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    },
+  },
   solidity: "0.8.4",
 };
